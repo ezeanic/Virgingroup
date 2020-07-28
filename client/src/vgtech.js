817d9b095542f6b554d7358ae1d80550784e10c0
ezeanic <ezeanic@uindy.edu>
2020-07-27 20:54:35 -0400

    updated linking to page using react router

---
+++ client/src/vgtech.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class vgtech extends React.Component{
+  render() {
+    return (
+      <div id="firstpagetext">
+          <h1>Understanding The Population With Special Needs</h1>
+          <p>Before you can learn how to better engage and provide a great environment for the population with special needs, 
+            you must first understand what the term special needs means and who is included when this term is used. 
+            During the “Understanding” lesson, you will learn just how large the population with special needs is, 
+            as well as the different types of diagnosis that are included in the term special needs.</p>
+          
+    </div>
+    );
+  }
+}
+
+export default vgtech;
